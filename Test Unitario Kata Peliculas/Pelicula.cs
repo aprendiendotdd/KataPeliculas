415766905765624153177dced189a9af037d981a
Francisco Calles <francisco.calles@hotmail.com>
2013-10-11 08:20:17 +0200

    Commit con los test de registros del fichero de entrada

--- Test Unitario Kata Peliculas/Pelicula.cs
+++ Test Unitario Kata Peliculas/Pelicula.cs
@@ -12,7 +12,58 @@ namespace Test_Unitario_Kata_Peliculas
         {
             string[] datos = texto.Split('|');
             ValidarIDDatos(datos[0]);
-            
+            ValidarTituloDatos(datos[1]);
+            ValidarDirectorDatos(datos[2]);
+            ValidarCantidadDatos(datos[3]);
+            ValidarPrecioDatos(datos[4]);           
+        }
+
+        private void ValidarPrecioDatos(string precio)
+        {
+            try
+            {
+                Precio = float.Parse(precio);
+            }
+            catch
+            {
+                throw new Exception("Precio no valido");
+            }
+        }
+
+        private void ValidarCantidadDatos(string cantidad)
+        {
+            try
+            {
+                Cantidad = int.Parse(cantidad);
+            }
+            catch
+            {
+                throw new Exception("Cantidad no numerico");
+            }
+        }
+
+        private void ValidarDirectorDatos(string director)
+        {
+            if ((0 < director.Trim().Length) || (director.Trim().Length < 51))
+            {
+                Director = director;
+            }
+            else
+            {
+                throw new Exception("Director no valido");
+            }
+        }
+
+        private void ValidarTituloDatos(string titulo)
+        {
+            if ((0 < titulo.Trim().Length) || (titulo.Trim().Length < 51))
+            {
+                Titulo = titulo;
+            }
+            else
+            {
+                throw new Exception("Titulo no valido");
+            }
         }
 
         private void ValidarIDDatos(string iD)
@@ -28,5 +79,13 @@ namespace Test_Unitario_Kata_Peliculas
         }
 
         public int ID { get; private set; }
+
+        public string Titulo { get; set; }
+
+        public string Director { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public float Precio { get; set; }
     }
 }
--- Test Unitario Kata Peliculas/Test_Sobre_Registros.cs
+++ Test Unitario Kata Peliculas/Test_Sobre_Registros.cs
@@ -12,5 +12,33 @@ namespace Test_Unitario_Kata_Peliculas
             Pelicula pelicula = new Pelicula("1|REGRESO AL FUTURO|ROBERT ZEMECKIS|3|9.99");
             Assert.AreEqual(1, pelicula.ID);         
         }
+
+        [TestMethod]
+        public void Validar_Titulo_Registro_leido_De_Fichero()
+        {
+            Pelicula pelicula = new Pelicula("1|REGRESO AL FUTURO|ROBERT ZEMECKIS|3|9.99");
+            Assert.AreEqual("REGRESO AL FUTURO", pelicula.Titulo);
+        }
+
+        [TestMethod]
+        public void Validar_Director_Registro_leido_De_Fichero()
+        {
+            Pelicula pelicula = new Pelicula("1|REGRESO AL FUTURO|ROBERT ZEMECKIS|3|9.99");
+            Assert.AreEqual("ROBERT ZEMECKIS", pelicula.Director);
+        }
+
+        [TestMethod]
+        public void Validar_Cantidad_Registro_leido_De_Fichero()
+        {
+            Pelicula pelicula = new Pelicula("1|REGRESO AL FUTURO|ROBERT ZEMECKIS|3|9.99");
+            Assert.AreEqual(3, pelicula.Cantidad);
+        }
+
+        [TestMethod]
+        public void Validar_Precio_Registro_leido_De_Fichero()
+        {
+            Pelicula pelicula = new Pelicula("1|REGRESO AL FUTURO|ROBERT ZEMECKIS|3|9,99");
+            Assert.AreEqual("9,99", pelicula.Precio.ToString());
+        }
     }
 }
